79b5263868a420c9904ac79f1bb54afce79a1b55
xer0x <drew.miller@auth0.com>
2019-12-21 22:09:31 -0800

    Fancy CAPSLOCK to CONTROL->ESCAPE

--- keyboards/wilba_tech/wt60_d/keymaps/n0rvig/keymap.c
+++ keyboards/wilba_tech/wt60_d/keymaps/n0rvig/keymap.c
@@ -3,6 +3,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+// Mod-tap CTRL->ESC via https://docs.qmk.fm/#/feature_advanced_keycodes
+#define CTRLESC LCTL_T(KC_ESC)
+
 // Default layer
 // top: ESC -> GESC w/ ` on Shift/GUI
 // 2: KC_LCTL -> MT(KC_ESC, KC_LCTL)
@@ -11,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_all(
 	KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+	CTRLESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSPO, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, MO(1),
 	KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, KC_RGUI, MO(1),   KC_RCTL),
 
--- keyboards/wilba_tech/wt60_d/rules.mk
+++ keyboards/wilba_tech/wt60_d/rules.mk
@@ -29,3 +29,11 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
+
+#TAP_DANCE_ENABLE=yes        # Tap dance for fancy CTRL-key dm
+
+RAW_ENABLE = yes
+DYNAMIC_KEYMAP_ENABLE = yes
+
+# project specific files
+SRC =	keyboards/wilba_tech/wt_main.c
